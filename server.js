937166ce58ba1a9f08e8ebe188b60accd0563d37
Ava Hurst <ava.hurst.home@gmail.com>
2024-04-11 18:49:09 +0200

    set up super simple express.js server

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-import bun from "bun";
 import express from "express"
+
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(3030, () => {
+    console.log("Listening on port 3030")
+})
+
+app.get("/", (req, res) => {
+    res.send('hello world')
+})
