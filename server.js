3bfee542b72c2058b05194b08d5a860a65aaf5ab
Ava Hurst <ava.hurst.home@gmail.com>
2024-04-12 00:50:02 +0200

    Added full server-side firebase authentication based off of the tutorial from Maksim Ivanov

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 import express from "express"
+import csrf from "csurf"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+import admin from "firebase-admin"
 
+const serviceAccount = require("./serviceAccountKey.json")
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+
+const csrfMiddleware = csrf({ cookie: true })
+
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.set("view engine", "ejs")
@@ -7,8 +20,13 @@ app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(3030, () => {
-    console.log("Listening on port 3030")
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(csrfMiddleware)
+
+app.all("*", (req, res, next) => {
+    res.cookie("CSRF-TOKEN", req.csrfToken())
+    next()
 })
 
 app.get(["/", "/index"], (req, res) => {
@@ -17,4 +35,55 @@ app.get(["/", "/index"], (req, res) => {
 
 app.get("/portfolio", (req, res) => {
     res.render('portfolio')
+})
+
+app.get("/signup", (req, res) => {
+    res.render('sign-up')
+})
+
+app.get("/login", (req, res) => {
+    res.render('login')
+})
+
+app.get("/profile", (req, res) => {
+    const sessionCookie = req.cookies.session || ""
+
+    admin
+        .auth()
+        .verifySessionCookie(sessionCookie, true /* checkRevoked */)
+        .then(() => {
+            res.render("profile")
+        })
+        .catch((error) => {
+            res.redirect("/login");
+        })
+})
+
+app.post("/sessionLogin", (req, res) => {
+    const idToken = req.body.idToken.toString()
+
+    const expiresIn = 60 * 60 * 24 * 5 * 1000
+
+    admin
+        .auth()
+        .createSessionCookie(idToken, { expiresIn })
+        .then(
+            (sessionCookie) => {
+                const options = { maxAge: expiresIn, httpOnly: true }
+                res.cookie("session", sessionCookie, options)
+                res.end(JSON.stringify({ status: "success" }))
+            },
+            (error) => {
+                res.status(401).send("UNAUTHORIZED REQUEST!")
+            }
+        )
+})
+
+app.get("/sessionLogout", (req, res) => {
+    res.clearCookie("session")
+    res.redirect("/login")
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
